136ba8cb52345318524fc3f906241c1e818d9ad6
Indra Arianggi Suryaatmaja <indraarianggi@gmail.com>
2025-05-20 02:51:59 +0700

    setup navigation, layouting, and HydrateFallback

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import {
-  Form,
+  Outlet,
   Scripts,
   ScrollRestoration,
   isRouteErrorResponse,
@@ -8,39 +8,8 @@ import type { Route } from "./+types/root";
 
 import appStylesHref from "./app.css?url";
 
-export default function App() {
-  return (
-    <>
-      <div id="sidebar">
-        <h1>React Router Contacts</h1>
-        <div>
-          <Form id="search-form" role="search">
-            <input
-              aria-label="Search contacts"
-              id="q"
-              name="q"
-              placeholder="Search"
-              type="search"
-            />
-            <div aria-hidden hidden={true} id="search-spinner" />
-          </Form>
-          <Form method="post">
-            <button type="submit">New</button>
-          </Form>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <a href={`/contacts/1`}>Your Name</a>
-            </li>
-            <li>
-              <a href={`/contacts/2`}>Your Friend</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </>
-  );
+export default function App({ loaderData }: Route.ComponentProps) {
+  return <Outlet />;
 }
 
 // The Layout component is a special export for the root route.
@@ -91,5 +60,15 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
         </pre>
       )}
     </main>
+  );
+}
+
+// Fallback that will show up before the app is hydrated (rendering on the client for the first time)
+export function HydrateFallback() {
+  return (
+    <div id="loading-splash">
+      <div id="loading-splash-spinner" />
+      <p>Loading, please wait...</p>
+    </div>
   );
 }
